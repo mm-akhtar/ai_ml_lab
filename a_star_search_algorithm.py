e826ded822e9c291aa1d25edd7cfe12a33de6d78
Md Masrur Akhtar <kkakhtar143@gmail.com>
2021-12-05 22:08:16 +0530

    a star search algorithm P-1

--- a_star_search_algorithm.py
+++ a_star_search_algorithm.py
@@ -23,20 +23,27 @@ class Graph:
         return H[n]
     
     def a_star_algorithm(self, start, stop):
-        
+        # In this open_lst is a list of nodes which have been visited, but who's 
+        # neighbours haven't all been always inspected, It starts off with the start 
+  #node
+        # And closed_lst is a list of nodes which have been visited
+        # and who's neighbors have been always inspected
         open_lst = set([start])
         close_lst = set([])
         
+        # poo has present distances from start to all other nodes
+        # the default value is +infinity
         poo = {}
         poo[start] = 0
         
+        # par contains an adjac mapping of all nodes
         par = {}
         par[start] = start
         
         while len(open_lst) > 0 :
             n = None
-            
             for v in open_lst:
+                # it will find a node with the lowest value of f() -
                 if n == None or poo[v] + self.h(v) < poo[n] + self.h(n):
                     n = v
                     
